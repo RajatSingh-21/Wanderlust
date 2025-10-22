96bf4bd71615a4841b3c9303ff515dc83c9730cc
Rajat Singh <rajatsingh22957@gmail.com>
2025-10-22 14:10:27 +0530

    change app.js

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const listingController = require("../controllers/listings.js");
 const multer = require("multer");
 const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });

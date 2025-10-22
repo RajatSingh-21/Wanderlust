23f0550a488af8ce4d494eb90ef22baa3c2173b0
Rajat Singh <rajatsingh22957@gmail.com>
2025-10-22 14:15:30 +0530

    change app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use((req, res, next) => {
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
-// app.use("/listings/category", categoryRouter);
+app.use("/listings/category", categoryRouter);
 
 app.get("/", (req, res) => {
   res.send("Server running successfully");

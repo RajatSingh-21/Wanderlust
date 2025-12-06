7fb973ebfb2b1645659584340f7b3a5a49b9ead4
Rajat Singh <rajatsingh22957@gmail.com>
2025-12-06 11:39:30 +0530

    updated

--- app.js
+++ app.js
@@ -94,9 +94,9 @@ app.use("/listings/category", categoryRouter);
 app.get("/listings", (req, res) => {
   res.send("Server running successfully");
 });
-// app.all("*", (req, res, next) => {
-//   next(new ExpressError(404, "Page not found"));
-// });
+app.all("*", (req, res, next) => {
+  next();
+});
 //ERROR HANDLING MIDDLEWARES
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "something went wrong" } = err;
